b85fc249a553022f7a369e527f22a8c810ea92d2
root <root@localhost>
2022-12-26 13:21:07 +0500

    auto publish 1672042867581

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x;
+  cout<<"please enter number = ";
+  cin>>x;
+  if(x>10){
+    cout<<endl<<"The number entered is greater";
+  }
+  else{
+    cout<<endl<<"The number entered is less";
+  }
+}
+
+     
+    
